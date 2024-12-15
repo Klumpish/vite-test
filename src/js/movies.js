ebcfe0620e1d824dc2a79e5be5b4e5720e2bae8c
Henrik <Henrikmattsson89@gmail.com>
2024-12-15 14:19:41 +0100

    test

---
+++ src/js/movies.js
@@ -0,0 +1,22 @@
+import movies from '../movies.json';
+
+// get a reference to my HTML container
+
+export default function movieCards() {
+  const moviesContainer = document.querySelector('.movies');
+
+  // generate html for each movie
+
+  movies.forEach((movie) => {
+    const movieElement = document.createElement('div');
+    movieElement.classList.add('movie');
+
+    movieElement.innerHTML = `
+        <h2>${movie.title}<h2>
+        <img src="${movie.image}" alt="${movie.title}">
+        <p>${movie.description}</p>
+        `;
+
+    moviesContainer.appendChild(movieElement);
+  });
+}
