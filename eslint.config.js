3f2cad085e076796221fc8a001515de3cc0b1143
Henrik <Henrikmattsson89@gmail.com>
2024-12-15 14:14:45 +0100

    maer

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import prettier from 'eslint-plugin-prettier';
 
 export default [
   {
-    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx', '**/*.html', '**/*.scss'],
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
     languageOptions: {
       ecmaVersion: 2021,
       sourceType: 'module',
