642c44afe624cca3a2ed6bfb69f3d18aceb397a4
Henrik <Henrikmattsson89@gmail.com>
2024-12-18 09:44:34 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/vite-test/', // Replace 'my-vite-app' with your repository name
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        aboutUs: './about-us.html',
+      },
+    },
+  },
 });
